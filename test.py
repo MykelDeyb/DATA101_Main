fe1f255380ffd757b10a0bea40a26eceb2ed2c22
MykelDeyb <164912075+MykelDeyb@users.noreply.github.com>
2024-03-26 09:50:49 +0800

    New Edit1

--- test.py
+++ test.py
@@ -2,4 +2,8 @@
 # git add .
 # git status
 # git commit -m "New Edit1"
-# git push
+# git push master
+
+#git init
+#git remote add origin https://github.com/MykelDeyb/DATA101_Main.git
+#git pull origin master --allow-unrelated-histories
