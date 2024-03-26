560d1e725f5e3ef376a61f7108f3003e7574ca52
MykelDeyb <164912075+MykelDeyb@users.noreply.github.com>
2024-03-26 14:20:46 +0800

    New Edit1

---
+++ INSTRUCTIONS.py
@@ -0,0 +1,12 @@
+# "How to Connect with Repository"
+#git init
+#git remote add origin https://github.com/MykelDeyb/DATA101_Main.git
+#git pull origin master --allow-unrelated-histories
+
+# "How to Pull Files from Repository and Push Files to Repository"
+# git pull origin master 
+# git add .
+# git status
+# git commit -m "New Edit1"
+# git push origin master
+
--- test.py
+++
@@ -1,9 +0,0 @@
-# git pull origin master 
-# git add .
-# git status
-# git commit -m "New Edit1"
-# git push origin master
-
-#git init
-#git remote add origin https://github.com/MykelDeyb/DATA101_Main.git
-#git pull origin master --allow-unrelated-histories
