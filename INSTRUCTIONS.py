c5ce28c68e850b1415ce5886e95c940e3da44b60
MykelDeyb <164912075+MykelDeyb@users.noreply.github.com>
2024-03-27 18:22:37 +0800

    New Edit1

--- INSTRUCTIONS.py
+++ INSTRUCTIONS.py
@@ -4,9 +4,9 @@
 #git pull origin master --allow-unrelated-histories
 
 # "How to Pull Files from Repository and Push Files to Repository"
-# git pull origin master 
-# git add .
-# git status
-# git commit -m "New Edit1"
-# git push origin master
+ git pull origin master 
+ git add .
+ git status
+ git commit -m "New Edit1"
+ git push origin master
 
